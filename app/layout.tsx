6e01fe4777503c61aec3bf5729f6603d47bee801
nggaaa <73785040+nggaaa@users.noreply.github.com>
2023-08-12 10:20:11 +0700

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Link from "next/link";
+import React from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
